55c71a379b4fa81d538f81e6d4e3490c3193ba6e
AriKrnwan <ari.20057@mhs.unesa.ac.id>
2024-05-18 10:44:23 +0700

    p

--- src/components/Wellcoming/wellcoming.jsx
+++ src/components/Wellcoming/wellcoming.jsx
@@ -12,15 +12,15 @@ function Wellcoming() {
                 <div className="text-wellcome">
                     <div className="text-top d-flex gap-1 align-items-center">
                         <div className="text-bg text-white px-2 py-1 rounded-2">
-                            <p className="ubuntu-sans-medium">Selamat Datang</p>
+                            <p className="ubuntu-sans-semibold">Selamat Datang</p>
                         </div>
                         <div className="text-nonbg text-white">
-                            <p className="ubuntu-sans-medium">di</p>
+                            <p className="ubuntu-sans-semibold">di</p>
                         </div>
                     </div>
-                    <div className="text-bottom ubuntu-sans-medium text-white">Platform Pelayanan Publik</div>
+                    <div className="custom-platform-text text-bottom ubuntu-sans-semibold text-white">Platform Pelayanan Publik</div>
                 </div>
-                <div className="long-text ubuntu-sans-regular text-white">Kami siap memberikan pelayanan yang cekatan, amanah, informatif, dan ramah. Kami hadir untuk memudahkan Anda dengan layanan yang handal dan penuh kepedulian.</div>
+                <div className="long-text ubuntu-sans-medium text-white">Kami siap memberikan pelayanan yang cekatan, amanah, informatif, dan ramah. Kami hadir untuk memudahkan Anda dengan layanan yang handal dan penuh kepedulian.</div>
             </div>
         </div>
         </>
